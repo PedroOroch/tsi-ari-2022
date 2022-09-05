daacd5067174da7e31d77f6705e4ece6546ea032
Oroch <oliveirarocha_pedro@hotmail.com>
2022-09-04 22:43:05 -0300

    ADO 1 FINALIZADA

--- aula03/exercicio01/script.js
+++ aula03/exercicio01/script.js
@@ -1,18 +1,30 @@
 let video = document.getElementById('vid');
 let meter = document.getElementById('meter');
 let tempoText = document.getElementById('tempoText');
+const BPLAY = document.querySelector('img');
+
+const VLRANGE = document.querySelector('input');
+const VLPROGRESS = document.querySelector('.volume-progress');
 
 
 
 
 
-function playVideo() {
-    video.play();
+function playPauseVideo() {
+    if (video.paused) {
+        video.play();
+        BPLAY.setAttribute('src', 'img/pause-solid-ciano.png');
+    } else {
+        video.pause();
+        BPLAY.setAttribute('src', 'img/play-solid-ciano.png');
+    }
 }
 
+/*
 function pauseVideo() {
     video.pause();
 }
+*/
 
 video.addEventListener('timeupdate', function() {
 
@@ -21,4 +33,14 @@ video.addEventListener('timeupdate', function() {
 
     tempoText.innerText = (Math.floor(meter.value / 60) + ":" + Math.ceil((meter.value % 60))) + "/" + (Math.floor(meter.max / 60) + ":" + Math.ceil((meter.max % 60)));
 
+})
+
+VLRANGE.addEventListener('input', function(e) {
+    VLPROGRESS.style.width = VLRANGE.value + '%';
+
+    video.volume = VLRANGE.value / 100;
+}, false);
+
+video.addEventListener('ended', function(e) {
+    BPLAY.setAttribute('src', 'img/rotate-left-solid-ciano.png');
 })
--- aula03/exercicio02/script.js
+++ aula03/exercicio02/script.js
@@ -1,22 +1,40 @@
 let audioplay = document.getElementById('audioPlay');
 const BPLAY = document.querySelector('img');
 
-const MUSIC = new Audio('aud/batman.org.mp3');
+let music = new Audio('aud/batman.org.mp3');
 
 let checkplay = false;
 
+/*
 function playAudio() {
 
     if (checkplay) {
         MUSIC.pause();
-        BPLAY.setAttribute('src', 'img/play-solid-ciano.png');
+        BPLAY.setAttribute('src', 'img/play-solid-batman.png');
         checkplay = false;
+
     } else {
         MUSIC.play();
         MUSIC.loop = false;
 
-        BPLAY.setAttribute('src', 'img/pause-solid-ciano.png');
+        BPLAY.setAttribute('src', 'img/pause-solid-batman.png');
         checkplay = true;
     }
 
 }
+
+*/
+
+function playPauseAudio() {
+    if (music.paused) {
+        music.play();
+        BPLAY.setAttribute('src', 'img/pause-solid-batman.png');
+    } else {
+        music.pause();
+        BPLAY.setAttribute('src', 'img/play-solid-batman.png');
+    }
+}
+
+music.addEventListener('ended', function(e) {
+    BPLAY.setAttribute('src', 'img/rotate-left-solid-batman.png');
+});
